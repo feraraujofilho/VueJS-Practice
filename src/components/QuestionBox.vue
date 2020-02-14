9bc2b8dace4b3ea7da06b583d21dbba1c8734e90
Fernando Araujo <feraraujofilho@gmail.com>
2020-02-14 23:04:13 +0100

    first commit

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template v-slot:lead>
+        {{ currentQuestion.question }}
+      </template>
+
+      <hr class="my-4" />
+
+      <b-list-group>
+        <b-list-group-item
+          v-for="answer in answers"
+          :key="answer"
+          @click="selectAnswer(answer)"
+          :class="[selectedAnswer === answer ? 'selected' : '']"
+        >
+          {{ answer }}
+        </b-list-group-item>
+      </b-list-group>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button @click="next" variant="success" href="#">Next </b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function
+  },
+  data() {
+    return {
+      selectedAnswer: null
+    };
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    }
+  },
+  watch: {
+      currentQuestion() {
+          this.selectedAnswer = null
+          this.shuffleAnswers()
+      }
+  },
+  methods: {
+    selectAnswer(answer) {
+      this.selectedAnswer = answer;
+      console.log(answer);
+    },
+    shuffleAnswers(){
+        let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+    }
+  },
+  mounted() {
+    console.log(this.currentQuestion);
+  }
+};
+</script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+
+.list-group-item:hover {
+  background: #eee;
+  cursor: pointer;
+}
+
+.btn {
+  margin: 0 5px;
+}
+
+.selected {
+  background-color: lightblue;
+}
+
+.correct {
+  background-color: lightgreen;
+}
+
+.wrong {
+  background-color: red;
+}
+</style>
